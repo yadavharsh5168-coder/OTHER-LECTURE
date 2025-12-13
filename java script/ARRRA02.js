abffb0bd4c0e518a396f3c4096e53f96271c7629
yadavharsh5168 <yadavharsh5168@gmail.com>
2025-12-13 20:10:07 +0530

    h

---
+++ java script/ARRRA02.js
@@ -0,0 +1,7 @@
+const arrr= [1,4,6,9,9]
+arrr.push(10)
+arrr.shift()
+arrr.unshift("how are you")
+console.log(arrr)
+crrr=[[1,7,9,8,0],[2,2,9,3,4,4]]
+console.log(crrr)
---
+++ java script/two diff loops.js
@@ -0,0 +1,5 @@
+//for of loop
+for(const iterator of elements)
+cout<<element
+for(Off)
+arr=[1,8,1,2,9]
