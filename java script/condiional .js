0006e0a49f928ddefa959cabc90f426571a07a07
yadavharsh5168 <yadavharsh5168@gmail.com>
2025-12-13 11:12:08 +0530

    har

---
+++ java script/condiional .js
@@ -0,0 +1,6 @@
+rating=1;
+if(rating==1)
+    console.log("Poor")
+else if(rating==2)console.log("below average");
+    else if (rating==3)console.log("average");
+    
---
+++ java script/loops.js
@@ -0,0 +1,4 @@
+for(i=1;i<=59;i++){}
+
+
+    console.log(i)}
